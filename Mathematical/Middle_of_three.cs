97291df9f95ef9d727d461f7c25b340ad55a70ba
Sachin Kumar Rana <sachinkrana45@gmail.com>
2025-02-09 10:52:14 +0530

    Basic

--- Mathematical/Middle_of_three.cs
+++ Mathematical/Middle_of_three.cs
@@ -13,15 +13,22 @@ Explanation: Since 518>300 and 518<978, so 518 is the middle element.
 using System;
 namespace Demo
 {
-	class Program
+    class Program
+    {
+        int middle(int a, int b, int c) 
 	{
-		int middle(int a, int b, int c) 
-		{
-			//code here
-			if(a<=b && b<=c || (c<=b && b<=a))
-				return b;
-			else if((b<=a && a<=c) || (c<=a && a<=b))
-				return a;
-			return c;
-		}
+	    //code here
+	    if(a<=b && b<=c || (c<=b && b<=a))
+	        return b;
+	    else if((b<=a && a<=c) || (c<=a && a<=b))
+	        return a;
+	    return c;
 	}
+    }
+}
+
+
+
+
+
+
